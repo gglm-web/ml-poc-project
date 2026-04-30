d08a9470f29c61ad7f091fe5511c4d38e2b79f9b
unknown <debardhugo@gmail.com>
2026-04-30 20:02:46 +0200

    Added an automatic download of datasets

--- scripts/importing_data.py
+++ scripts/importing_data.py
@@ -1,13 +1,24 @@
 import kagglehub
 import os
+import shutil
 
-
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-data_dir = os.path.join(project_root, "data")
-
-path = kagglehub.dataset_download(
+datasets = [
     "uom190346a/sleep-health-and-lifestyle-dataset",
-    output_dir=data_dir
-)
+    "arashnic/fitbit"
+]
 
+data_dir = "data"
+
+os.makedirs(data_dir, exist_ok=True)
+
+for ds in datasets:
+    print(f"Téléchargement du dataset : {ds}")
+    src = kagglehub.dataset_download(ds)
+    dataset_name = ds.split('/')[-1]
+    dest = os.path.join(data_dir, dataset_name)
+    if os.path.exists(dest):
+        print(f"Suppression de l'ancien dossier : {dest}")
+        shutil.rmtree(dest)
+    shutil.move(src, dest)
+    print(f"Dataset sauvegardé dans : {dest}\n")
 
